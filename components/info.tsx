83d2b6225e0a8a5bab328109f984994813ad6ded
Beny.K <Beku1997@gmail.com>
2023-07-20 18:37:59 +0300

    page fix

--- components/info.tsx
+++ components/info.tsx
@@ -1,25 +1,24 @@
-import { Product } from '@/types'
-import Currency from './ui/currency'
-import Button from './ui/button'
+"use client";
 
-import {ShoppingCart} from "lucide-react"
-import useCart from '@/hooks/use-cart'
-import { MouseEventHandler } from 'react'
+import { ShoppingCart } from "lucide-react";
+
+import Currency  from "@/components/ui/currency";
+import Button from "@/components/ui/button";
+import { Product } from "@/types";
+import useCart from "@/hooks/use-cart";
 
 interface InfoProps {
   data: Product
-}
+};
 
 const Info: React.FC<InfoProps> = ({ data }) => {
-
-  const cart = useCart()
+  const cart = useCart();
 
   const onAddToCart = () => {
-    cart.addItem(data)
+    cart.addItem(data);
   }
 
-
-  return (
+  return ( 
     <div>
       <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
       <div className="mt-3 flex items-end justify-between">
@@ -27,30 +26,27 @@ const Info: React.FC<InfoProps> = ({ data }) => {
           <Currency value={data?.price} />
         </p>
       </div>
-      <div className='flex flex-col gap-y-6'>
       <hr className="my-4" />
-      <div className="flex items-center gap-x-4">
-        <h3 className="font-semibold text-black">Size:</h3>
-        <div className='font-bold'>{data?.size?.name}</div>
-      </div>
-      <div className="flex items-center gap-x-4">
-        <h3 className="font-semibold text-black">Color:</h3>
-        <div
-          className="h-6 w-6 rounded-full border border-gray-600"
-          style={{
-            backgroundColor: data?.color?.value,
-          }}
-        />
+      <div className="flex flex-col gap-y-6">
+        <div className="flex items-center gap-x-4">
+          <h3 className="font-semibold text-black">Size:</h3>
+          <div>
+            {data?.size?.value}
+          </div>
+        </div>
+        <div className="flex items-center gap-x-4">
+          <h3 className="font-semibold text-black">Color:</h3>
+          <div className="h-6 w-6 rounded-full border border-gray-600" style={{ backgroundColor: data?.color?.value }} />
         </div>
       </div>
-      <div className='mt-10 flex items-center gap-x-3'>
-          <Button className='flex items-center gap-x-2' onClick={onAddToCart}>
-            Add To Cart
-            <ShoppingCart/>
-          </Button>
+      <div className="mt-10 flex items-center gap-x-3">
+        <Button onClick={onAddToCart} className="flex items-center gap-x-2">
+          Add To Cart
+          <ShoppingCart size={20} />
+        </Button>
       </div>
     </div>
-  )
+  );
 }
-
-export default Info
+ 
+export default Info;
